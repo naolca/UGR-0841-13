b26ef50db41ca3939212d3742a1e823acff4f457
naolca <firaolibrahim28@gmail.com>
2023-01-16 16:35:03 +0300

    first commit
    this branch is the update of the main branch with only html.it adds the css and  javascript.

---
+++ src/mainHomePage.js
@@ -0,0 +1,39 @@
+const hamburger = document.querySelector(".hamburger");
+const $form = document.querySelector(".js-form");
+const $searchInput = document.querySelector(".js-search-input");
+hamburger.addEventListener("click", function () {
+  navBar = document.querySelector(".top__navbar");
+  navBar.classList.toggle("active");
+});
+
+const highlightError = ($input, errorMessage) => {
+  $input.parentNode.classList.add("form__field--error");
+  //add the css for this class.
+  $input.nextElementSibling.innerText = errorMessage;
+};
+
+const clearError = ($input) => {
+  $input.parentNode.classList.remove("form__field--error");
+  //add the css for this class.
+  $input.nextElementSibling.innerText = "";
+};
+
+const validate = () => {
+  searchInputValue = $searchInput.value.trim();
+
+  clearError($searchInput);
+
+  if (!searchInputValue) {
+    highlightError($searchInput, "seach field cannot be empty");
+  }
+
+  if (searchInputValue) {
+    window.location.href = "http://127.0.0.1:5500/searchResults.html";
+  }
+};
+
+$form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  validate();
+});
